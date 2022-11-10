91445a154995c5c638230d14ff22e4a97170256b
eagrcode <elliot.robinson92@hotmail.com>
2022-11-10 15:19:55 +0000

    add handle submit to Search component

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,12 +1,26 @@
+import { useRef } from "react";
+
 import { FaSearch } from "react-icons/fa";
 
 function Search() {
+  const searchInputRef = useRef();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const input = searchInputRef.current.value;
+    if (input === "") return;
+    console.log(input);
+  }
+
   return (
     <div className="search-container">
       <form>
-        <input type="text" placeholder="Enter borough name to get started" />
-
-        <button className="search-btn">
+        <input
+          ref={searchInputRef}
+          type="text"
+          placeholder="Enter borough name to get started"
+        />
+        <button onClick={handleSubmit} className="search-btn">
           <FaSearch className="search-icon" size={25} />
         </button>
       </form>
