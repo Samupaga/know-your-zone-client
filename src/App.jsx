70bec06a72c4c092fcadf3b6dfc5ad32a9a00db9
eagrcode <elliot.robinson92@hotmail.com>
2022-11-10 16:33:07 +0000

    remove comps from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,8 @@
 import "./normalize.css";
 import "./styles.css";
 
-import { Navbar, SkylineContainer, London } from "./components";
-
 function App() {
-  return (
-    <>
-      <Navbar />
-      <SkylineContainer />
-      <London />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
