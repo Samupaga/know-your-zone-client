b3791471a9de598b42f294058db67b0fc57c3408
emmamarks27 <emma_marks27@outlook.com			git config --global user.name emmamarks27>
2022-11-10 17:28:48 +0000

    Build the summary page

--- src/pages/SummaryPage/index.jsx
+++ src/pages/SummaryPage/index.jsx
@@ -1,47 +1,61 @@
-import { BigNumberCard, CardHIP, CardHPP } from '../../components';
+import {
+  BigNumberCard,
+  CardHIP,
+  CardHPP,
+  Navbar,
+  Container,
+  InnerNav,
+} from '../../components';
 import './summary.css';
 
 export function SummaryPage() {
-  console.log('hi from summary');
   return (
-    <div className='six-tile-wrapper'>
-      <BigNumberCard
-        className={'pink six-tile'}
-        value={1200}
-        smallNumber={'pcm'}
-        secondaryInfo={'Average Rent'}
-      />
-      <CardHIP
-        className={'yellow six-tile'}
-        heading={'Demographics'}
-        imageSrc={
-          'https://www.voxco.com/wp-content/uploads/2021/03/Demographic-Segmentation-1.jpg'
-        }
-        altImageText={'People standing on a pie chart'}
-      />
-      <BigNumberCard
-        className={'pink six-tile'}
-        value={87}
-        smallNumber={'/1000'}
-        secondaryInfo={'Average Crime Rate'}
-      />
-      <CardHPP
-        className={'yellow six-tile'}
-        heading={'Rent'}
-        primaryInfo={'⬇️'}
-        secondaryInfo={'Below London Average'}
-      />
-      <CardHPP
-        className={'pink six-tile'}
-        primaryInfo={'😎'}
-        secondaryInfo={'7.2 on the wellbeing score!'}
-      />
-      <CardHPP
-        className={'yellow six-tile'}
-        heading={'Crime'}
-        primaryInfo={'⬇️'}
-        secondaryInfo={'Below London Average'}
-      />
+    <div className='summary-wrapper'>
+      <Navbar />
+      <h1>Wandsworth</h1>
+      <h3 className='motto'>
+        <em>"We Serve"</em>
+      </h3>
+      <InnerNav />
+      <div className='six-tile-wrapper'>
+        <BigNumberCard
+          className={'pink six-tile'}
+          value={1200}
+          smallNumber={'pcm'}
+          secondaryInfo={'Average Rent'}
+        />
+        <CardHIP
+          className={'yellow six-tile'}
+          heading={'Demographics'}
+          imageSrc={
+            'https://www.voxco.com/wp-content/uploads/2021/03/Demographic-Segmentation-1.jpg'
+          }
+          altImageText={'People standing on a pie chart'}
+        />
+        <BigNumberCard
+          className={'pink six-tile'}
+          value={87}
+          smallNumber={'/1000'}
+          secondaryInfo={'Average Crime Rate'}
+        />
+        <CardHPP
+          className={'yellow six-tile'}
+          heading={'Rent'}
+          primaryInfo={'⬇️'}
+          secondaryInfo={'Below London Average'}
+        />
+        <CardHPP
+          className={'pink six-tile'}
+          primaryInfo={'😎'}
+          secondaryInfo={'7.2 on the wellbeing score!'}
+        />
+        <CardHPP
+          className={'yellow six-tile'}
+          heading={'Crime'}
+          primaryInfo={'⬇️'}
+          secondaryInfo={'Below London Average'}
+        />
+      </div>
     </div>
   );
 }
