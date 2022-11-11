0807b85693a43768d7e217b02cbc17256926c12c
eagrcode <elliot.robinson92@hotmail.com>
2022-11-11 17:44:58 +0000

    add starting responsiveness to summary page

--- src/components/InnerNav/InnerNav.jsx
+++ src/components/InnerNav/InnerNav.jsx
@@ -1,31 +1,33 @@
-import { React, useState } from 'react';
-import { InnerNavTab } from '../';
+import { React, useState } from "react";
+import { InnerNavTab } from "../";
 
 const InnerNav = () => {
   return (
-    <nav className='tab-container'>
-      <InnerNavTab
-        page={'Summary'}
-        routeName={'/borough/summary'}
-        activeState={true}
-      />
-      <InnerNavTab
-        page={'Rent Prices'}
-        routeName={'/borough/rent'}
-        activeState={false}
-      />
-      <InnerNavTab
-        page={'Crime'}
-        routeName={'/borough/crime'}
-        activeState={false}
-      />
-      <InnerNavTab page={'Wellbeing'} activeState={false} />
-      <InnerNavTab
-        page={'Demographics'}
-        routeName={'/borough/demographics'}
-        activeState={false}
-      />
-    </nav>
+    <div className="inner-nav-container">
+      <nav className="tab-container">
+        <InnerNavTab
+          page={"Summary"}
+          routeName={"/borough/summary"}
+          activeState={true}
+        />
+        <InnerNavTab
+          page={"Rent Prices"}
+          routeName={"/borough/rent"}
+          activeState={false}
+        />
+        <InnerNavTab
+          page={"Crime"}
+          routeName={"/borough/crime"}
+          activeState={false}
+        />
+        <InnerNavTab page={"Wellbeing"} activeState={false} />
+        <InnerNavTab
+          page={"Demographics"}
+          routeName={"/borough/demographics"}
+          activeState={false}
+        />
+      </nav>
+    </div>
   );
 };
 
