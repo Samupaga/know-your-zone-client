51476df95787c4f066bef26702c55e0c0df31de8
eagrcode <elliot.robinson92@hotmail.com>
2022-11-11 10:40:44 +0000

    add HomePage component

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,18 @@
+import { Navbar, SkylineContainer, London } from "../../components";
+
+function HomePage() {
+  return (
+    <>
+      <Navbar />
+      <div className="hero-container">
+        <h1 className="home-title">Know Your Zone</h1>
+        <p>Everything you need to know about London Boroughs in one place</p>
+
+        <SkylineContainer />
+        <London />
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
