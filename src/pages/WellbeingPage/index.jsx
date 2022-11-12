0d3a037cf4b79fb361746669b5a53a4ea72bbbd5
“ayesha-24” <ayesharahim24@gmail.com>
2022-11-12 19:53:35 +0000

    fixed route for wellbeing page

--- src/pages/WellbeingPage/index.jsx
+++ src/pages/WellbeingPage/index.jsx
@@ -2,12 +2,7 @@ import { CardHPP, CardWellbeing, InnerNav } from "../../components";
 import "./wellbeing.css";
 import { useState, useEffect } from "react";
 
-export default function WellbeingPage() {
-  let worthwhileScore = 7.71;
-  let happinessScore = 7.5;
-  let anxietyScore = 3.15;
-  let lifeSatisfactionScore = 7.65;
-
+export default function WellbeingPage({ navSearchSearching }) {
   const [isLoading, setIsLoading] = useState(true);
   const [wellbeingData, setWellbeingData] = useState([]);
 
@@ -15,18 +10,21 @@ export default function WellbeingPage() {
   useEffect(() => {
     async function getBoroughInfo() {
       setIsLoading(true);
-      const response = await fetch(`http://localhost:3000/summary/${boroughName}`);
+      const response = await fetch(`http://localhost:3000/demographics/${boroughName}/wellbeing`);
       const rawData = await response.json();
       setWellbeingData(rawData);
+      setIsLoading(false);
     }
 
     getBoroughInfo();
   }, [navSearchSearching]);
 
+  // console.log("wellbeing data", wellbeingData);
+
   if (isLoading === false) {
     return (
       <div className="page-wrapper">
-        <h1>Wandsworth</h1>
+        <h1>{wellbeingData["borough_name"]}</h1>
         <h3 className="motto">
           <em>"We Serve"</em>
         </h3>
@@ -37,25 +35,25 @@ export default function WellbeingPage() {
               <CardHPP
                 className={"pink four-tile"}
                 heading={"Worthwhile"}
-                primaryInfo={`${worthwhileScore}`}
+                primaryInfo={`${wellbeingData["worthwhile"]}`}
                 secondaryInfo={"This is a measure of to what extent residents feel the things they do in their life are worthwhile, out of 10."}
               />
               <CardHPP
                 className={"blue four-tile"}
                 heading={"Happiness"}
-                primaryInfo={`${happinessScore}`}
+                primaryInfo={`${wellbeingData["happiness"]}`}
                 secondaryInfo={"This is a measure of to what extent residents feel happy in their day to day life, out of 10."}
               />
               <CardHPP
                 className={"blue four-tile"}
                 heading={"Anxiety"}
-                primaryInfo={`${anxietyScore}`}
+                primaryInfo={`${wellbeingData["anxiety"]}`}
                 secondaryInfo={"This is a measure of to what extent residents feel anxious in their day to day life, out of 10."}
               />
               <CardHPP
                 className={"pink four-tile"}
                 heading={"Life Satisfaction"}
-                primaryInfo={`${lifeSatisfactionScore}`}
+                primaryInfo={`${wellbeingData["life_satisfaction"]}`}
                 secondaryInfo={"This is a measure of to what extent residents are satisfied with how their life is, out of 10."}
               />
             </div>
