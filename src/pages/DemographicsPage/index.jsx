5e47cf53cf87ec03dd68a88f8009cd03d5842c54
“ayesha-24” <ayesharahim24@gmail.com>
2022-11-14 09:44:12 +0000

    not found page created

--- src/pages/DemographicsPage/index.jsx
+++ src/pages/DemographicsPage/index.jsx
@@ -35,6 +35,20 @@ export default function DemographicsPage({ navSearchSearching }) {
     getBoroughInfo();
   }, [navSearchSearching]);
 
+  let maxSpeed = {
+    religionData,
+  };
+  let sortable = [];
+  for (var vehicle in maxSpeed) {
+    sortable.push([vehicle, maxSpeed[vehicle]]);
+  }
+
+  sortable.sort(function (a, b) {
+    return a[1] - b[1];
+  });
+
+  // [["bike", 60], ["motorbike", 200], ["car", 300],
+  // ["helicopter", 400], ["airplane", 1000], ["rocket", 28800]]
   console.log("religiondata", religionData);
 
   const getGreeting = (lang) => {
