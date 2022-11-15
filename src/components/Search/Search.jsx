64000185b375f06023c9a1995516b9812224f7a2
eagrcode <elliot.robinson92@hotmail.com>
2022-11-15 10:58:05 +0000

    cleanup search dropdown menu

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,52 +1,52 @@
-import { useRef, useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { FaSearch } from 'react-icons/fa';
+import { useRef, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
 
 function Search() {
-  const searchInputRef = useRef('');
+  const searchInputRef = useRef("");
   const navigate = useNavigate();
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
   const [filteredBoroughList, setFilteredBoroughList] = useState([]);
 
   const boroughList = [
-    'Barking and Dagenham',
-    'Barnet',
-    'Bexley',
-    'Brent',
-    'Bromley',
-    'Camden',
-    'Croydon',
-    'Ealing',
-    'Enfield',
-    'Greenwich',
-    'Hackney',
-    'Hammersmith and Fulham',
-    'Haringey',
-    'Harrow',
-    'Havering',
-    'Hillingdon',
-    'Hounslow',
-    'Islington',
-    'Kensington and Chelsea',
-    'Kingston upon Thames',
-    'Lambeth',
-    'Lewisham',
-    'Merton',
-    'Newham',
-    'Redbridge',
-    'Richmond upon Thames',
-    'Southwark',
-    'Sutton',
-    'Tower Hamlets',
-    'Waltham Forest',
-    'Wandsworth',
-    'Westminster',
+    "Barking and Dagenham",
+    "Barnet",
+    "Bexley",
+    "Brent",
+    "Bromley",
+    "Camden",
+    "Croydon",
+    "Ealing",
+    "Enfield",
+    "Greenwich",
+    "Hackney",
+    "Hammersmith and Fulham",
+    "Haringey",
+    "Harrow",
+    "Havering",
+    "Hillingdon",
+    "Hounslow",
+    "Islington",
+    "Kensington and Chelsea",
+    "Kingston upon Thames",
+    "Lambeth",
+    "Lewisham",
+    "Merton",
+    "Newham",
+    "Redbridge",
+    "Richmond upon Thames",
+    "Southwark",
+    "Sutton",
+    "Tower Hamlets",
+    "Waltham Forest",
+    "Wandsworth",
+    "Westminster",
   ];
 
   function searchBoroughList() {
     let query = searchInputRef.current.value;
     setSearchQuery(query);
-    console.log('search query', searchQuery);
+    console.log("search query", searchQuery);
     setFilteredBoroughList(
       boroughList.filter((borough) =>
         borough.toLowerCase().includes(searchQuery.toLowerCase())
@@ -55,9 +55,9 @@ function Search() {
   }
 
   function searchClick(e) {
-    console.log('search click called');
+    console.log("search click called");
     filteredBoroughList[0] = e.target.id;
-    sessionStorage.setItem('borough', filteredBoroughList[0]);
+    sessionStorage.setItem("borough", filteredBoroughList[0]);
     navigate(`/borough/summary`);
   }
 
@@ -67,29 +67,29 @@ function Search() {
     //if (searchQuery === '') return;
     try {
       // Save data to sessionStorage
-      sessionStorage.setItem('borough', filteredBoroughList[0]);
+      sessionStorage.setItem("borough", filteredBoroughList[0]);
       navigate(`/borough/summary`);
     } catch (error) {
-      navigate('/error', { state: { message: 'Failed to submit form' } });
+      navigate("/error", { state: { message: "Failed to submit form" } });
     }
   }
 
   if (filteredBoroughList.length != 0) {
     return (
-      <div className='search-container'>
-        <form className='big-form' onSubmit={handleSubmit}>
+      <div className="search-container">
+        <form className="big-form" onSubmit={handleSubmit}>
           <input
-            className='big-input'
+            className="big-input"
             ref={searchInputRef}
             onChange={searchBoroughList}
-            type='text'
-            placeholder='Enter a borough to get started'
+            type="text"
+            placeholder="Enter a borough to get started"
           />
-          <button type='submit' className='search-btn'>
-            <FaSearch className='search-icon' size={25} />
+          <button type="submit" className="search-btn">
+            <FaSearch className="search-icon" size={25} />
           </button>
         </form>
-        <div className='dropdown'>
+        <ul className="dropdown">
           {boroughList
             .filter((borough) =>
               borough.toLowerCase().includes(searchQuery.toLowerCase())
@@ -103,22 +103,22 @@ function Search() {
                 {borough}
               </a>
             ))}
-        </div>
+        </ul>
       </div>
     );
   } else {
     return (
-      <div className='search-container'>
-        <form className='big-form' onSubmit={handleSubmit}>
+      <div className="search-container">
+        <form className="big-form" onSubmit={handleSubmit}>
           <input
-            className='big-input'
+            className="big-input"
             ref={searchInputRef}
             onChange={searchBoroughList}
-            type='text'
-            placeholder='Enter a borough to get started'
+            type="text"
+            placeholder="Enter a borough to get started"
           />
-          <button type='submit' className='search-btn'>
-            <FaSearch className='search-icon' size={25} />
+          <button type="submit" className="search-btn">
+            <FaSearch className="search-icon" size={25} />
           </button>
         </form>
       </div>
