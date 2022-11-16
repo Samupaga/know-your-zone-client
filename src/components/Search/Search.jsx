98d50813784ca33ccc1ddab5bc4f79a8f572f519
emmamarks27 <emma_marks27@outlook.com			git config --global user.name emmamarks27>
2022-11-16 13:16:27 +0000

    Add icons on summary page

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,52 +1,52 @@
-import { useRef, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { FaSearch } from "react-icons/fa";
+import { useRef, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { FaSearch } from 'react-icons/fa';
 
 function Search() {
-  const searchInputRef = useRef("");
+  const searchInputRef = useRef('');
   const navigate = useNavigate();
-  const [searchQuery, setSearchQuery] = useState("");
+  const [searchQuery, setSearchQuery] = useState('');
   const [filteredBoroughList, setFilteredBoroughList] = useState([]);
 
   const boroughList = [
-    "Barking and Dagenham",
-    "Barnet",
-    "Bexley",
-    "Brent",
-    "Bromley",
-    "Camden",
-    "Croydon",
-    "Ealing",
-    "Enfield",
-    "Greenwich",
-    "Hackney",
-    "Hammersmith and Fulham",
-    "Haringey",
-    "Harrow",
-    "Havering",
-    "Hillingdon",
-    "Hounslow",
-    "Islington",
-    "Kensington and Chelsea",
-    "Kingston upon Thames",
-    "Lambeth",
-    "Lewisham",
-    "Merton",
-    "Newham",
-    "Redbridge",
-    "Richmond upon Thames",
-    "Southwark",
-    "Sutton",
-    "Tower Hamlets",
-    "Waltham Forest",
-    "Wandsworth",
-    "Westminster",
+    'Barking and Dagenham',
+    'Barnet',
+    'Bexley',
+    'Brent',
+    'Bromley',
+    'Camden',
+    'Croydon',
+    'Ealing',
+    'Enfield',
+    'Greenwich',
+    'Hackney',
+    'Hammersmith and Fulham',
+    'Haringey',
+    'Harrow',
+    'Havering',
+    'Hillingdon',
+    'Hounslow',
+    'Islington',
+    'Kensington and Chelsea',
+    'Kingston upon Thames',
+    'Lambeth',
+    'Lewisham',
+    'Merton',
+    'Newham',
+    'Redbridge',
+    'Richmond upon Thames',
+    'Southwark',
+    'Sutton',
+    'Tower Hamlets',
+    'Waltham Forest',
+    'Wandsworth',
+    'Westminster',
   ];
 
   function searchBoroughList() {
     let query = searchInputRef.current.value;
     setSearchQuery(query);
-    console.log("search query", searchQuery);
+    console.log('search query', searchQuery);
     setFilteredBoroughList(
       boroughList.filter((borough) =>
         borough.toLowerCase().includes(searchQuery.toLowerCase())
@@ -55,38 +55,38 @@ function Search() {
   }
 
   function searchClick(e) {
-    console.log("search click called");
+    console.log('search click called');
     filteredBoroughList[0] = e.target.id;
-    sessionStorage.setItem("borough", filteredBoroughList[0]);
+    sessionStorage.setItem('borough', filteredBoroughList[0]);
     navigate(`/borough/summary`);
   }
 
   function handleSubmit(e) {
     e.preventDefault();
 
-    if (searchQuery === "") return;
+    if (searchQuery === '') return;
 
     // Save data to sessionStorage
-    sessionStorage.setItem("borough", filteredBoroughList[0]);
+    sessionStorage.setItem('borough', filteredBoroughList[0]);
     navigate(`/borough/summary`);
   }
 
   if (filteredBoroughList.length != 0) {
     return (
-      <div className="search-container">
-        <form className="big-form" onSubmit={handleSubmit}>
+      <div className='search-container'>
+        <form className='big-form' onSubmit={handleSubmit}>
           <input
-            className="big-input"
+            className='big-input'
             ref={searchInputRef}
             onChange={searchBoroughList}
-            type="text"
-            placeholder="Enter a borough to get started"
+            type='text'
+            placeholder='Enter a borough to get started'
           />
-          <button type="submit" className="search-btn">
-            <FaSearch className="search-icon" size={25} />
+          <button type='submit' className='search-btn'>
+            <FaSearch className='search-icon' size={25} />
           </button>
         </form>
-        <ul className="dropdown">
+        <ul className='dropdown'>
           {boroughList
             .filter((borough) =>
               borough.toLowerCase().includes(searchQuery.toLowerCase())
@@ -106,17 +106,17 @@ function Search() {
     );
   } else {
     return (
-      <div className="search-container">
-        <form className="big-form" onSubmit={handleSubmit}>
+      <div className='search-container'>
+        <form className='big-form' onSubmit={handleSubmit}>
           <input
-            className="big-input"
+            className='big-input'
             ref={searchInputRef}
             onChange={searchBoroughList}
-            type="text"
-            placeholder="Enter a borough to get started"
+            type='text'
+            placeholder='Enter a borough to get started'
           />
-          <button type="submit" className="search-btn">
-            <FaSearch className="search-icon" size={25} />
+          <button type='submit' className='search-btn'>
+            <FaSearch className='search-icon' size={25} />
           </button>
         </form>
       </div>
--- src/pages/SummaryPage/index.jsx
+++ src/pages/SummaryPage/index.jsx
@@ -1,11 +1,22 @@
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { motion, AnimatePresence } from 'framer-motion';
+import {
+  BsEmojiSmile,
+  BsFillArrowUpSquareFill,
+  BsFillArrowDownSquareFill,
+} from 'react-icons/bs';
+import {
+  BigNumberCard,
+  CardHIP,
+  CardHPP,
+  Navbar,
+  Container,
+  InnerNav,
+} from '../../components';
 
-import { motion, AnimatePresence } from "framer-motion";
-
-import { BigNumberCard, CardHIP, CardHPP, Navbar, Container, InnerNav } from "../../components";
-
-import "./summary.css";
+import './summary.css';
 
 export default function SummaryPage({ navSearchSearching }) {
   const [isLoading, setIsLoading] = useState(true);
@@ -15,22 +26,24 @@ export default function SummaryPage({ navSearchSearching }) {
   const navigate = useNavigate();
   //For later use - fetch request example
   // Get saved data from sessionStorage
-  let boroughName = sessionStorage.getItem("borough");
+  let boroughName = sessionStorage.getItem('borough');
   useEffect(() => {
     async function getBoroughInfo() {
       setIsLoading(true);
       try {
-        const response = await fetch(`http://localhost:3000/summary/${boroughName}`);
+        const response = await fetch(
+          `http://localhost:3000/summary/${boroughName}`
+        );
         const rawData = await response.json();
         setBoroughData(rawData);
         // setMotto(rawData['motto']);
         // console.log('raw data', rawData);
 
-
-        const wellbeingResponse = await fetch(`http://localhost:3000/demographics/${boroughName}/wellbeing`);
+        const wellbeingResponse = await fetch(
+          `http://localhost:3000/demographics/${boroughName}/wellbeing`
+        );
         const wellbeingData = await wellbeingResponse.json();
         setWellbeingScore(wellbeingData['data'][4]['value']);
-
 
         setIsLoading(false);
       } catch (err) {
@@ -52,48 +65,76 @@ export default function SummaryPage({ navSearchSearching }) {
           animate={{ x: 0, opacity: 1 }}
           exit={{ x: -300, opacity: 0 }}
         >
-          <div className="six-tile-wrapper">
+          <div className='six-tile-wrapper'>
             <BigNumberCard
-              className={"yellow six-tile"}
-              value={`£${boroughData["average_monthly_rent"]}`}
-              smallNumber={"pcm"}
-              secondaryInfo={"Average Rent"}
+              className={'yellow six-tile'}
+              value={`£${boroughData['average_monthly_rent']}`}
+              smallNumber={'pcm'}
+              secondaryInfo={'Average Rent'}
             />
             <CardHIP
-              className={"blue six-tile"}
-              heading={"Demographics"}
-              imageSrc={"https://www.voxco.com/wp-content/uploads/2021/03/Demographic-Segmentation-1.jpg"}
-              altImageText={"People standing on a pie chart"}
+              className={'blue six-tile'}
+              heading={'Demographics'}
+              imageSrc={
+                'https://www.voxco.com/wp-content/uploads/2021/03/Demographic-Segmentation-1.jpg'
+              }
+              altImageText={'People standing on a pie chart'}
             />
             <BigNumberCard
-              className={"pink six-tile"}
-              value={Math.floor(boroughData["crime_rate_per_1000"])}
-              smallNumber={"/1000"}
-              secondaryInfo={"Average Crime Rate"}
+              className={'pink six-tile'}
+              value={Math.floor(boroughData['crime_rate_per_1000'])}
+              smallNumber={'/1000'}
+              secondaryInfo={'Average Crime Rate'}
             />
             <CardHPP
-              className={"yellow six-tile"}
-              heading={"Rent"}
-              primaryInfo={`${boroughData["rent_below_london_average"] === true ? "⬇️" : "⬆️"}`}
-              secondaryInfo={`${boroughData["rent_below_london_average"] === true ? "Below London Average" : "Above London Average"}`}
+              className={'yellow six-tile'}
+              heading={'Rent'}
+              primaryInfo={
+                boroughData['rent_below_london_average'] ? (
+                  <BsFillArrowDownSquareFill />
+                ) : (
+                  <BsFillArrowUpSquareFill />
+                )
+              }
+              secondaryInfo={`${
+                boroughData['rent_below_london_average'] === true
+                  ? 'Below London Average'
+                  : 'Above London Average'
+              }`}
             />
             <CardHPP
-              className={"blue six-tile"}
-              primaryInfo={`${wellbeingScore > 7.3 ? "😎" : "🙂"}`}
+              className={'blue six-tile'}
+              heading={'Wellbeing'}
+              primaryInfo={`${wellbeingScore > 7.3 ? '😎' : '🙂'}`}
               secondaryInfo={`${wellbeingScore} on the wellbeing score!`}
             />
             <CardHPP
-              className={"pink six-tile"}
-              heading={"Crime"}
-              primaryInfo={`${boroughData["crime_below_london_average"] ? "⬇️" : "⬆️"}`}
-              secondaryInfo={`${boroughData["crime_below_london_average"] ? "Below London Average" : "Above London Average"}`}
+              className={'pink six-tile'}
+              heading={'Crime'}
+              primaryInfo={
+                boroughData['crime_below_london_average'] ? (
+                  <BsFillArrowDownSquareFill />
+                ) : (
+                  <BsFillArrowUpSquareFill />
+                )
+              }
+              secondaryInfo={`${
+                boroughData['crime_below_london_average']
+                  ? 'Below London Average'
+                  : 'Above London Average'
+              }`}
             />
           </div>
         </motion.div>
       </AnimatePresence>
     );
   } else if (boroughFound === false) {
-    return <h3>We are really sorry, the borough you have entered has not been found. You will now be redirected back to the home page.</h3>;
+    return (
+      <h3>
+        We are really sorry, the borough you have entered has not been found.
+        You will now be redirected back to the home page.
+      </h3>
+    );
   } // else {
   //   return (
   //     <div className="page-wrapper">
@@ -107,3 +148,13 @@ export default function SummaryPage({ navSearchSearching }) {
   //   );
   // }
 }
+
+// {
+//   `${
+//     wellbeingScore > 7.3 ? (
+//       <FontAwesomeIcon icon='fa-regular fa-face-smile-beam' />
+//     ) : (
+//       '🙂'
+//     )
+//   }`;
+// }
