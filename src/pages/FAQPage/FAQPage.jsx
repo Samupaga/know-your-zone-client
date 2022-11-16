078c82c75d889df0f600851db2648d45b311dbe9
“ayesha-24” <ayesharahim24@gmail.com>
2022-11-16 16:02:51 +0000

    outline of faq

--- src/pages/FAQPage/FAQPage.jsx
+++ src/pages/FAQPage/FAQPage.jsx
@@ -1,15 +1,18 @@
 function FAQPage() {
   return (
     <div>
-      <h1>FQADFQFAwfawfa</h1>
-      <h1>FQADFQFAwfawfa</h1>
-      <h1>FQADFQFAwfawfa</h1>
-      <h1>FQADFQFAwfawfa</h1>
-      <h1>FQADFQFAwfawfa</h1>
-      <h1>FQADFQFAwfawfa</h1>
-      <h1>FQADFQFAwfawfa</h1>
-      <h1>FQADFQFAwfawfa</h1>
-      <h1>FQADFQFAwfawfa</h1>
+      <h1>About 'Know Your Zone'</h1>
+      <p>Know Your Zone is a website made to show the necessary information about all of the London boroughs, all in one place.</p>
+      <p>Our team has collated data from different sources to bring you the most up to date information on the borough.</p>
+      <p>Sources of data:</p>
+      <ul>
+        <li>source</li>
+        <li>source</li>
+        <li>source</li>
+        <li>source</li>
+        <li>source</li>
+        <li>source</li>
+      </ul>
     </div>
   );
 }
