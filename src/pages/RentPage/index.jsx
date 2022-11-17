b2f860f0b7f16021dcb3db5dfa8353aa23011c7e
“ayesha-24” <ayesharahim24@gmail.com>
2022-11-17 11:24:13 +0000

    rent page fix

--- src/pages/RentPage/index.jsx
+++ src/pages/RentPage/index.jsx
@@ -13,14 +13,7 @@ export default function RentPage({ navSearchSearching, motto }) {
   const xAxisTitle = "Period Covering 2018/09 to 2022/03";
   const yAxisTitle = "£ per Month";
 
-  const londonLabels = [
-    "2018/09-2019/09",
-    "2019/03-2020/03",
-    "2019/09-2020/09",
-    "2020/03-2021/03",
-    "2020/09-2021/09",
-    "2021/03-2022/03",
-  ];
+  const londonLabels = ["2018/09-2019/09", "2019/03-2020/03", "2019/09-2020/09", "2020/03-2021/03", "2020/09-2021/09", "2021/03-2022/03"];
   //For later use - fetch request example
   // Get saved data from sessionStorage
   let boroughName = sessionStorage.getItem("borough");
@@ -32,9 +25,7 @@ export default function RentPage({ navSearchSearching, motto }) {
       setAverageRent(averageRentPrice[0]);
       setRentHistory(averageRentPrice);
 
-      const responseTwo = await fetch(
-        `http://localhost:3000/rent/${boroughName}/accommodation`
-      );
+      const responseTwo = await fetch(`http://localhost:3000/rent/${boroughName}/accommodation`);
       const generalRentInfo = await responseTwo.json();
       setGeneralRent(generalRentInfo);
 
@@ -47,12 +38,7 @@ export default function RentPage({ navSearchSearching, motto }) {
   if (isLoading === false) {
     return (
       <AnimatePresence>
-        <motion.div
-          initial={{ x: 300, opacity: 0 }}
-          animate={{ x: 0, opacity: 1 }}
-          exit={{ x: -300, opacity: 0 }}
-          className="rent-tile-wrapper"
-        >
+        <motion.div initial={{ x: 300, opacity: 0 }} animate={{ x: 0, opacity: 1 }} exit={{ x: -300, opacity: 0 }} className="rent-tile-wrapper">
           <CardHIP
             className={"left-column card blue rent-item-1"}
             imageSrc={
@@ -60,51 +46,27 @@ export default function RentPage({ navSearchSearching, motto }) {
             }
             altImageText={"Gantt Chart"}
             heading={"Median Rent"}
-            secondaryInfo={
-              "Median rent paid across all house types for the last 4 years"
-            }
+            secondaryInfo={"Median rent paid across all house types for the last 4 years"}
             chartType={"line"}
             londonLabels={londonLabels}
             dataResponse={rentHistory.reverse().map((elem) => elem.rent_median)}
             xAxisTitle={xAxisTitle}
             yAxisTitle={yAxisTitle}
           />
-          <CardHIP
-            className={"right-column card blue rent-item-2"}
-            chartType={"donut"}
-            heading={"Split of some data"}
-          />
+          <CardHIP className={"right-column card blue rent-item-2"} chartType={"donut"} heading={"Split of some data"} />
 
           <BigNumberCard
             className={"left-column card navy rent-item-3"}
             value={`£${averageRent["rent_median"]}`}
             smallNumber={"pcm"}
-            secondaryInfo={
-              "is the average rent price across all accommodation types"
-            }
+            secondaryInfo={"is the average rent price across all accommodation types"}
           />
 
           <div className="four-tile-wrapper right-column">
-            <CardHP
-              className={"card pink four-tile rent-item-4"}
-              heading={`£${generalRent[0]["rent_median"]}`}
-              secondaryInfo={"Single Room"}
-            />
-            <CardHP
-              className={"card pink four-tile rent-item-5"}
-              heading={`£${generalRent[5]["rent_median"]}`}
-              secondaryInfo={"1 Bed"}
-            />
-            <CardHP
-              className={"card pink four-tile rent-item-6"}
-              heading={`£${generalRent[4]["rent_median"]}`}
-              secondaryInfo={"3 Bed"}
-            />
-            <CardHP
-              className={"card pink four-tile rent-item-7"}
-              heading={`£${generalRent[2]["rent_median"]}`}
-              secondaryInfo={"4+ Bed"}
-            />
+            <CardHP className={"card pink four-tile rent-item-4"} heading={`£${generalRent[2]["rent_median"]}`} secondaryInfo={"Single Room"} />
+            <CardHP className={"card pink four-tile rent-item-5"} heading={`£${generalRent[1]["rent_median"]}`} secondaryInfo={"1 Bed"} />
+            <CardHP className={"card pink four-tile rent-item-6"} heading={`£${generalRent[4]["rent_median"]}`} secondaryInfo={"3 Bed"} />
+            <CardHP className={"card pink four-tile rent-item-7"} heading={`£${generalRent[0]["rent_median"]}`} secondaryInfo={"4+ Bed"} />
           </div>
         </motion.div>
       </AnimatePresence>
